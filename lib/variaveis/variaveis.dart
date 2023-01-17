8d15be7c4029ab0f1bb7ba1092d9a6182c3b9522
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2023-01-16 21:27:05 -0300

    feat: :sparkles: add variaveis

---
+++ lib/variaveis/variaveis.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  num qualquerNumero = 1;
+  num qualquerNumero2 = 1.2;
+
+  int idade = 37;
+
+  idade + 1 == 38;
+
+  bool ativo = true;
+
+  double valorIphone;
+  valorIphone = 12.000;
+
+  String nomeCurso = 'Academia do Flutter';
+
+  var name = 'Nome da Pessoa';
+  var diaAniversario = 13;
+
+  var salario = 100.000;
+  // A value of type 'String' can't be assigned to a variable of type 'double'.
+  // salario = '';
+
+  // todas as classes herdam de Object
+  Object objetoQualquer = 1.2;
+  objetoQualquer = 'qualquer coisa';
+
+  dynamic qualquerCoisa = 1.2;
+  qualquerCoisa = 'qualquer coisa';
+  qualquerCoisa = qualquerCoisa + ' que vc quiser!';
+  qualquerCoisa = 1;
+  qualquerCoisa = qualquerCoisa + 1 == 2;
+  qualquerCoisa = false;
+
+  const int number = 1;
+  // number = 2;
+}
